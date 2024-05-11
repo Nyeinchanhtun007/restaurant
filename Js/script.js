842f701ee0d0ca2f16c4b7ad6a5cc9fd21284efd
Nyeinchanhtun007 <rainestarboylay@gmail.com>
2024-05-11 15:36:39 +0630

    resturant

---
+++ Js/script.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll(".tab");
+const tabBtns = document.querySelectorAll(".tab-btn");
+
+const tab_nav = function (tabBtnClick) {
+  tabBtns.forEach((tabBtn) => {
+    tabBtn.classList.remove("active");
+  });
+
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  tabBtns[tabBtnClick].classList.add("active");
+  tabs[tabBtnClick].classList.add("active");
+};
+
+tabBtns.forEach((tabBtn, i) => {
+  tabBtn.addEventListener("click", () => {
+    tab_nav(i);
+  });
+});
